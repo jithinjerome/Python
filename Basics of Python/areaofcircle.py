07907f45d636a240cf40da1c274f9de0c8187f2d
Jithin Jerome <97607784+jithinjerome@users.noreply.github.com>
2023-08-21 07:12:42 +0530

    Area of circle

---
+++ Basics of Python/areaofcircle.py
@@ -0,0 +1,10 @@
+#***********************************************************************
+#CREATOR : JITHIN JEROME
+#TITLE - SIMPLE INTREST
+#DATE - AUGUST , 2023
+#***********************************************************************
+def area(r):
+    area = 3.14*r*r
+    print("Area is %.2f"% area)
+r= int(input("Enter the radius of the circle: "))
+area(r)
